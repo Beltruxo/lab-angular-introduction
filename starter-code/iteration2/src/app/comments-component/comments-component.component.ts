6f1758e5d8d1317a47f27f06470559876fccc03a
Beltran <beltran_xv@hotmail.com>
2018-05-01 17:57:23 +0200

    Sin maquetar

--- starter-code/iteration2/src/app/comments-component/comments-component.component.ts
+++ starter-code/iteration2/src/app/comments-component/comments-component.component.ts
@@ -4,12 +4,48 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-comments-component',
   templateUrl: './comments-component.component.html',
   styleUrls: ['./comments-component.component.css']
+  
 })
 export class CommentsComponentComponent implements OnInit {
+name:string,
+content:string,
 
   constructor() { }
 
   ngOnInit() {
   }
+comments = [
+  {
+    name: "Belt",
+    content: "Life is Life"
+  },
+  {
+    name: "Alejandro",
+    content: "All you need is love"
+  },
+  {
+    name: "Tran",
+    content: "Live Hard"
+  },
+  {
+    name: "Ale",
+    content: "Play Rugby"
+  },
+  {
+    name: "Jandro",
+    content: "Die Ugly"
+  },
+          
+]
 
+recordAllTheKeyStrokes(event) {
+  console.log(`Key inserted: ${event.key}`);
+  //console.log(`Input value: ${event.currentTarget.value}`);
+}
+
+addComment(name, content){
+  this.comments.push({name, content})
+  this.name = []
+  this.content = []
+}
 }
